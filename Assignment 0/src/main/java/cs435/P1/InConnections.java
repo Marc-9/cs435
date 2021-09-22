6931b88842a19bd6391c287d16f7d87d9212981e
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2021-09-22 07:31:56 -0600

    create files for in and out connections sorted in 2 jobs

--- Assignment 0/src/main/java/cs435/P1/InConnections.java
+++ Assignment 0/src/main/java/cs435/P1/InConnections.java
@@ -2,9 +2,6 @@ package cs435.P1;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,26 +9,21 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.Arrays;
-import java.util.List;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Comparator;
 import java.util.ArrayList;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
 public class InConnections {
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+    public static class OutMapper extends Mapper<Object, Text, Text, IntWritable> {
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             List<String> test = Arrays.asList(value.toString().split(" "));
             context.write(new Text(test.get(0)), new IntWritable(1));
         }
     }
-    public static class CountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+    public static class OutReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
         List<Edge> sortedList;
         @Override
         protected void setup(Context context) {
@@ -84,10 +76,10 @@ public class InConnections {
         @Override
         public int compare(Edge o1, Edge o2){
             if (o1.count > o2.count){
-                return 1;
+                return -1;
             }
             else if(o1.count < o2.count){
-                return -1;
+                return 1;
             }
             else{
                 return o1.key.compareTo(o2.key);
@@ -95,70 +87,73 @@ public class InConnections {
         }
     }
 
-    public static class UselessMapper extends Mapper<Object, Text, Text, Text> {
+    public static class InMapper extends Mapper<Object, Text, Text, IntWritable> {
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            context.write(new Text("0"), value);
+            List<String> test = Arrays.asList(value.toString().split(" "));
+            context.write(new Text(test.get(1)), new IntWritable(1));
         }
     }
-
-    public static class SortReducer extends Reducer<Text, Text, Text, IntWritable> {
-        private Map < Text, IntWritable > inNodes;
+    public static class InReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+        List<Edge> sortedList;
         @Override
         protected void setup(Context context) {
-            inNodes = new HashMap < Text, IntWritable >();
+            sortedList = new ArrayList<Edge>();
         }
-        @Override
-        protected void reduce(Text key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
-            for(Text val : value){
-                List<String> test = Arrays.asList(val.toString().split("	"));
-                inNodes.put(new Text(test.get(0)), new IntWritable(Integer.parseInt(test.get(1))));
-            }
 
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) {
+            int count = 0;
+            // calculate the total count
+            for(IntWritable val : values){
+                count += val.get();
+            }
+            //context.write(key, new IntWritable(count));
+            sortedList.add(new Edge(key, count));
         }
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
-            LinkedHashMap<Text, IntWritable> reverseSortedMap = new LinkedHashMap<>();
-            inNodes.entrySet()
-                    .stream()
-                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                    .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
-            for(Map.Entry<Text, IntWritable> entry : reverseSortedMap.entrySet()) {
-                context.write(entry.getKey(), entry.getValue());
+            Collections.sort(sortedList, new CustomComparator());
+            //Arrays.sort(sortedList);
+            for(int i = 0; i < sortedList.size(); i++) {
+                context.write(new Text(sortedList.get(i).key), new IntWritable(sortedList.get(i).count));
             }
         }
     }
+
+
 
 
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "in connections");
+        Job job = Job.getInstance(conf, "out connections");
         job.setJarByClass(InConnections.class);
-        job.setMapperClass(InConnections.TokenizerMapper.class);
-        job.setReducerClass(InConnections.CountReducer.class);
-
+        job.setMapperClass(InConnections.OutMapper.class);
+        job.setReducerClass(InConnections.OutReducer.class);
+        job.setNumReduceTasks(1);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[1]));
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
         job.waitForCompletion(true);
 
         Configuration conf2 = new Configuration();
-        Job job2 = Job.getInstance(conf, "sort node");
+        Job job2 = Job.getInstance(conf, "in connections");
         job2.setJarByClass(InConnections.class);
-        job2.setMapperClass(InConnections.UselessMapper.class);
-        job2.setReducerClass(InConnections.SortReducer.class);
-
+        job2.setMapperClass(InConnections.InMapper.class);
+        job2.setReducerClass(InConnections.InReducer.class);
+        job2.setNumReduceTasks(1);
         job2.setMapOutputKeyClass(Text.class);
-        job2.setMapOutputValueClass(Text.class);
+        job2.setMapOutputValueClass(IntWritable.class);
         job2.setOutputKeyClass(Text.class);
         job2.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job2, new Path(args[2]));
+        FileInputFormat.addInputPath(job2, new Path(args[1]));
         FileOutputFormat.setOutputPath(job2, new Path(args[3]));
+
         System.exit(job2.waitForCompletion(true) ? 0 : 1);
 
     }
